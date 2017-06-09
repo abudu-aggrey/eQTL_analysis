8c5b6d5cb60f05ecf6180b071ef84ffbbaf6585e
abudu-aggrey <ashley.budu-aggrey@bristol.ac.uk>
2017-06-09 18:19:33 +0100

    Create run_coloc.R

--- run_coloc.R
+++ run_coloc.R
@@ -1,34 +1,37 @@
 #generate input files for coloc analysis.
 
-#read in results from eQTL analysis and pull out rsID, beta and se
+#read in results from eQTL analysis 
 
 eQTL_data <- read.table("./TwinsUK/CD207_eQTL_analysis_all.assoc.txt", header=T)
-eQTL_data <- eQTL_data[ ,c(2,8,9)]
-colnames(eQTL_data)[1] <- "rsID"
+
+#define region around leand SNP
+SNP_pos <- eQTL_data$ps[which(eQTL_data$rs == "rs112111458")]
+upp_pos <- SNP_pos + 100000
+low_pos <- SNP_pos - 100000
+
 
 #read in GWAS data and subset for SNPs from eQTL analysis
 ECZ_GWAS <- read.table("./EAGLE_AD_GWAS/EAGLE_AD_GWAS_results_2015.txt", header=T)
-GWAS_data <- merge(ECZ_GWAS, eQTL_data, by="rsID")
-
-#extract snp, beta, se and eaf
-GWAS_data <- GWAS_data[ ,c(1, 8,9,6)]
-colnames(GWAS_data)[2] <- "beta"
-colnames(GWAS_data)[3] <- "se"
-
-#exclude SNPs in expression data with no genotype data
-colnames(eQTL_data)[2] <- "exp_beta"
-colnames(eQTL_data)[3] <- "exp_se"
-eQTL_GWAS_merge <- merge(eQTL_data, GWAS_data, by = "rsID")
-eQTL_data <- eQTL_GWAS_merge[ ,1:3]
+GWAS_data <- ECZ_GWAS[which(ECZ_GWAS$rsID %in% eQTL_data$rs), ]
+locus_snps <- GWAS_data[which(GWAS_data$position > low_pos & GWAS_data$position < upp_pos), ]
+locus_eQTL <- eQTL_data[which(eQTL_data$rs %in% locus_snps$rsID), ]
 
 
 #Perform coloc
 #install.packages("coloc")
 library(coloc)
 
-my.res <- coloc.abf(dataset1=list(beta=GWAS_data$beta, varbeta=GWAS_data$se, N=nrow(GWAS_data), snp=GWAS_data$rsID, type="quant"),
-            dataset2=list(beta=eQTL_data$exp_beta, varbeta=eQTL_data$exp_se, N=nrow(eQTL_data), snp=eQTL_data$rsID, type="quant"),
-            MAF=GWAS_data$eaf)
+#N = 102762
+#N minus 23andme = 102762 - 62231 = 40531
+#ratio of cases to controls = 0.150
+#using p-values
+
+my.res <- coloc.abf(dataset1=list(pvalues=locus_snps$p.value, N=40531, snp=locus_snps$rsID, type="cc", s=0.150),
+            dataset2=list(pvalues=locus_eQTL$p_score, N=672, snp=locus_eQTL$rs, type="quant"),
+            MAF=locus_eQTL$af)
+
 
 coloc_results <- my.res$results
 coloc_results <- coloc_results[order(coloc_results$SNP.PP.H4,decreasing=T),]
+
+write.table(coloc_results, file="./TwinsUK/coloc/results_CD207_minus_23andme.txt", quote=F, sep=" ", row.names=F)
